6b9c07827b6ede2938ab1ba65feb5ff9559bed03
smpahk <smpahk@etri.re.kr>
2015-11-09 16:57:03 +0900

    Hello Lithium Blank Template.

---
+++ impl/src/main/java/kr/re/etri/tsdn/impl/HelloProvider.java
@@ -0,0 +1,29 @@
+/*
+ * copyright 2015 ETRI all rights reserved. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package kr.re.etri.tsdn.impl;
+
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloProvider implements BindingAwareProvider, AutoCloseable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HelloProvider.class);
+
+    @Override
+    public void onSessionInitiated(ProviderContext session) {
+        LOG.info("HelloProvider Session Initiated");
+    }
+
+    @Override
+    public void close() throws Exception {
+        LOG.info("HelloProvider Closed");
+    }
+
+}
